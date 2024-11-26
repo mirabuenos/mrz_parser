ffbe004dd0f527b029d5c50fd74d9442b6e567d6
mirabuenos <71399969+mirabuenos@users.noreply.github.com>
2024-11-26 10:46:13 +0100

    Update mrz_parser_esp.dart

--- lib/mrz_parser_esp.dart
+++ lib/mrz_parser_esp.dart
@@ -1,3 +1,13 @@
+library mrz_parser_esp;
+
 export 'src/mrz_exceptions.dart';
-export 'src/mrz_parser.dart';
 export 'src/mrz_result.dart';
+export 'src/sex.dart';
+
+part 'src/mrz_parser.dart';
+part 'src/mrz_string_extensions.dart';
+part 'src/mrz_field_parser.dart';
+part 'src/mrz_field_recognition_defects_fixer.dart';
+part 'src/td1_format_mrz_parser.dart';
+part 'src/td2_format_mrz_parser.dart';
+part 'src/td3_format_mrz_parser.dart';
